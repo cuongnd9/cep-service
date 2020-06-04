a6ecc8d3fb3b83f367df79b562a9a0a218e2ece5
Cuong Duy Nguyen <103cuong@gmail.com>
2020-06-05 01:17:42 +0700

    Add migration

--- src/migrations/20200605162303_define_database.js
+++ src/migrations/20200605162303_define_database.js
@@ -1,29 +1,10 @@
-import { DataTypes } from 'sequelize';
-
 const migration = {
-  up: (queryInterface) => queryInterface.createTable('cats', {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    color: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    updated_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-  }),
+  up: (queryInterface, Sequelize) => {
+    const sql = '';
+    return queryInterface.sequelize.query(sql, {
+      type: Sequelize.QueryTypes.RAW,
+    });
+  },
 };
 
 export default migration;
