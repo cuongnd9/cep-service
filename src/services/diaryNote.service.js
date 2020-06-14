fe759bc6782e3f174ecdfacaab9f4119b0937ccf
Cuong Duy Nguyen <103cuong@gmail.com>
2020-06-15 00:58:39 +0700

    Fix diaryNotes query (#14)

--- src/services/diaryNote.service.js
+++ src/services/diaryNote.service.js
@@ -2,21 +2,28 @@ import { Op } from 'sequelize';
 import { uniq, flatten } from 'lodash';
 
 import db from '../models';
-import { DIARY_FILTER } from '../components/constants';
+import { DIARY_FILTER, POLICY } from '../components/constants';
 
 class DiaryNoteService {
   static async getDiaryNotes(data) {
     const {
       offset, limit, userId, filter,
     } = data;
-    if (filter === DIARY_FILTER.byAccount) {
-      return db.DiaryNote.findAndCountAll({
+    let diariesByAccount;
+    let diariesByRelative;
+    if (filter === DIARY_FILTER.byAccount || filter === DIARY_FILTER.all) {
+      diariesByAccount = await db.DiaryNote.findAndCountAll({
         where: {
           createdBy: userId,
         },
         limit,
         offset,
       });
+      if (filter === DIARY_FILTER.byAccount) {
+        return {
+          byAccount: diariesByAccount,
+        };
+      }
     }
     const relationships = await db.Relationship.findAll({
       where: {
@@ -35,30 +42,28 @@ class DiaryNoteService {
       flatten(
         relationships.map((item) => [item.userId1, item.userId2]),
       ),
-    ).map((item) => item !== userId);
-    if (filter === DIARY_FILTER.byRelative) {
-      return db.DiaryNote.findAndCountAll({
+    ).filter((item) => item !== userId);
+    if (filter === DIARY_FILTER.byRelative || filter === DIARY_FILTER.all) {
+      diariesByRelative = await db.DiaryNote.findAndCountAll({
         where: {
           createdBy: {
             [Op.in]: relativeIds,
           },
+          policy: POLICY.public,
         },
         limit,
         offset,
       });
+      if (filter === DIARY_FILTER.byRelative) {
+        return {
+          byRelative: diariesByRelative,
+        };
+      }
     }
-    return db.DiaryNote.findAndCountAll({
-      where: {
-        createdBy: {
-          [Op.in]: [
-            ...relativeIds,
-            userId,
-          ],
-        },
-      },
-      limit,
-      offset,
-    });
+    return {
+      byAccount: diariesByAccount,
+      byRelative: diariesByRelative,
+    };
   }
 
   static createDiaryNote(data) {
