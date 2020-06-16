6f91a3940ae596c97ef3e21654502975ae2231c8
Cuong Duy Nguyen <103cuong@gmail.com>
2020-06-16 18:03:48 +0700

    Update account.resolver.js

--- src/resolvers/account.resolver.js
+++ src/resolvers/account.resolver.js
@@ -5,22 +5,10 @@ import AccountService from '../services/account.service';
 
 const resolver = {
   Query: {
-    login: middleware(
-      validateSchema({
-        // password: joi.string().min(8).max(30),
-        phone: joi.string().min(10).max(11),
-      }),
-      (_, args) => AccountService.login(args),
-    ),
+    login: (_, args) => AccountService.login(args),
   },
   Mutation: {
-    register: middleware(
-      validateSchema({
-        // password: joi.string().min(8).max(30),
-        phone: joi.string().min(10).max(11),
-      }),
-      (_, args) => AccountService.register(args),
-    ),
+    register: (_, args) => AccountService.register(args),
   },
 };
 
