af5c3e3cea7c7499bfa1990060dc767c98cd7a80
Cuong Duy Nguyen <103cuong@gmail.com>
2020-07-13 01:05:29 +0700

    Add script files for dev-env (#20)

--- src/components/config.js
+++ src/components/config.js
@@ -1,7 +1,7 @@
 const config = {
   nodeEnv: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 4000,
-  pgHost: process.env.PG_HOST || 'db',
+  pgHost: process.env.PG_HOST || '127.0.0.1',
   pgPort: process.env.PG_PORT || 5432,
   pgDB: process.env.PG_DB || 'cep-service',
   pgUser: process.env.PG_USER || 'postgres',
